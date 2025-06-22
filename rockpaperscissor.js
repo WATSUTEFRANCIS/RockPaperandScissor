63f959abadd4e40ff378ba5c4b823c4a8de1964f
watsutefrancis <watsutefrancis61@gmail.com>
2025-06-22 16:11:00 +0300

    ADD:function of playround and ensured results are prodeuced in console

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -21,5 +21,28 @@ function getComputerChoice() {
   let humanScore=0;
   let computerScore=0;
 
+  function playRound(humanChoice, computerChoice) {
+    humanChoice=humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+      return " You draw";
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      humanScore++;
+      return `You win! ${humanChoice} beats ${computerChoice}.`;
+    } else {
+      computerScore++;
+      return `You lose! ${computerChoice} beats ${humanChoice}.`;
+    }
+    }
+
+    const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+ const result=playRound(humanSelection, computerSelection);
+
+console.log(result);
+console.log(`Score: You ${humanScore} - ${computerScore} Computer`);
 
 
